104a8d3c8a6344ea4000660e7020b7a6009d201b
Grabiel De La Nieve <grabieldelanieve@gmail.com>
2019-03-31 17:03:45 -0400

    Adaptacion completa

---
+++ htmlHelpersFormulario/htmlHelpersFormulario/Controllers/PersonaController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using htmlHelpersFormulario.Models;
+
+namespace htmlHelpersFormulario.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET : /User/Index
+        public ActionResult Index()
+        {
+            return View(new Persona());
+        }
+
+
+        public ActionResult Profile(Persona user)
+        {
+            return View(user);
+        }
+
+        // GET : /User/Create
+        public ActionResult Create()
+        {
+            return View(new Persona());
+        }
+
+        [HttpPost]
+        // GET : /User/Create
+        public ActionResult Create(Persona user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+            return View("Profile", user);
+        }
+    }
+}
---
+++ htmlHelpersFormulario/htmlHelpersFormulario/Models/Persona.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace htmlHelpersFormulario.Models
+{
+    public class Persona
+    {
+        [DisplayName("Cedula")]
+        [MinLength(10)]
+        [Required]
+        public string IdNational { get; set; }
+
+        [DisplayName("Nombre")]
+        [MinLength(10)]
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Apellido")]
+        [MinLength(10)]
+        [Required]
+        public string LastName { get; set; }
+
+        [DisplayName("Correo Electronico")]
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [DisplayName("Clave")]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+
+        [DisplayName("Edad")]
+        [Range(15, 100)]
+        public int Age { get; set; }
+
+        [DisplayName("Estado Civil")]
+        public string Status { get; set; }
+
+        [DisplayName("Carrera")]
+        public string Career { get; set; }
+
+        [DisplayName("Numero De Telefono")]
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [DisplayName("Hobbys")]
+        public string[] Hobbys { get; set; }
+
+        [DisplayName("Género:")]
+        public string Genero { get; set; }
+    }
+}
